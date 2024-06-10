38fa49efd4e270a314c5629e5e4084b477eb2788
mmbics <phoeyarzar.ygn@gmail.com>
2024-06-11 00:07:09 +0630

    modify console messages in index.js

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 console.log('Hello World!');
 console.log("This is a new line.");
 
-alert("Hello World!");
+alert("Hello Earth! Hello Mars!");
 
-console.log("Hello World");
+console.log("Hello Multiple Lines!");
 
 function sayHello() {
     console.log("Hello World");
